8116b72d7a5ea3cb73655dcceb07ca591b7e895a
YashUmak <www.yashumak@gmail.com>
2025-07-15 09:11:38 +0530

    Update 0.4

--- src/utils/ga4.js
+++ src/utils/ga4.js
@@ -1,7 +1,9 @@
 import ReactGA from 'react-ga4';
 
 const TRACKING_ID = import.meta.env.VITE_GA_TRACKING_ID;
-ReactGA.initialize(TRACKING_ID);
+if (TRACKING_ID) {
+  ReactGA.initialize(TRACKING_ID);
+}
 
 export const trackPageView = (page) => {
   ReactGA.send({ hitType: 'pageview', page });
